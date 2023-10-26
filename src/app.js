6d8cc36716bb57fae6031607419bcb557b9a33c8
BrunoCalace <bruno.calace@gmail.com>
2023-10-25 23:26:28 -0300

    primerEntregaIntegradoraCalace - V1.2

--- src/app.js
+++ src/app.js
@@ -1,34 +1,5 @@
 import express from 'express'
 import productsRouter from './router/productsRouter.js'
-import handlebars from 'express-handlebars'
-import mongoose from 'mongoose'
-import __dirname from './utils.js'
-
-const app = express()
-const mongoURL = 'mongodb+srv://brunocalace:QT2q9bemAvh5n658@clustercalace.yrqgvm7.mongodb.net/'
-const mongoDBName = 'ecommerce'
-
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
-
-app.engine('handlebars', handlebars.engine())
-app.set('views', __dirname + '/views')
-app.set('view engine', 'handlebars')
-
-app.use(express.static( __dirname + '/public'))
-
-app.get('/health', (req, res) => res.send('ok'))
-app.use('/products', productsRouter)
-
-mongoose.connect(mongoURL, {dbName: mongoDBName })
-    .then(() => {
-        console.log('DB connected')
-        app.listen(8080, () => console.log('Listening...'))
-    })
-    .catch(error => {
-        console.error('Error connect DB')
-    })import express from 'express'
-import productsRouter from './router/productsRouter.js'
 import chatRouter from './router/chatRouter.js'
 import handlebars from 'express-handlebars'
 import mongoose from 'mongoose'
